d5a2f700b6c6abfeb8a46d52044706e403c508a9
nikmorror37 <nikitamironchuk37@gmail.com>
2023-01-31 22:15:01 +0300

    commit1(program.cs)

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,21 +12,27 @@ namespace Test
             int a = 23;
             string str = "Slovo";
             bool b = true;
+
+            int c = 18;
+            int d = 5;
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine(str);
             a = a + 23;
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(a);
+            //Console.WriteLine("Hello, World!(1)");//
+            Console.WriteLine("Hello, World!(2)");
 
             string json = JsonConvert.SerializeObject(str);
 
+            Console.Write("Multiply of numbers:");
+            Console.WriteLine(c * d);
             Console.Write("Hello");
-            Console.WriteLine("World!");
+            Console.WriteLine(" World!(3)");
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
-            Console.Write("Good day, ");
+            Console.Write("Learn and practise, ");
             Console.Write(name);
             Console.WriteLine(":))");
 
