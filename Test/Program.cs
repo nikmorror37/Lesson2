9bb3f20dbf16ee99d95075b4f7273471ad2b527d
nikmorror37 <nikitamironchuk37@gmail.com>
2023-02-07 16:26:47 +0300

    Commit_Lesson3_HT

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,74 +9,211 @@ namespace Test
     {
         static void Main(string[] args) 
         {
-            int a = 23;
-            string str = "Slovo";
-            bool b = true;
 
-            double c = 18;
-            int d = 5;
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine(str);
-            a = a + 23;
-            Console.WriteLine(a);
-            //Console.WriteLine("Hello, World!(1)");//
-            Console.WriteLine("Hello, World!(2)");
+            /*      //задание ДЗ №5
+            
+            Console.Write("Enter the color's number of rainbow: ");
 
-            string json = JsonConvert.SerializeObject(str);
+            int a = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Multiply of numbers:");
-            Console.WriteLine(c * d);
-            Console.WriteLine(c / d);
-            Console.Write("Hello");
-            Console.WriteLine(" World!(3)");
-           
-            /*
-              Example of comments
-             */
-
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();  // вводим имя
-            if (name == "")
+            switch (a)
             {
-                Console.WriteLine("Empty name");
+                case 1:
+                    Console.WriteLine("Red");
+                    break;
+                case 2:
+                    Console.WriteLine("Orange");
+                    break;
+                case 3:
+                    Console.WriteLine("Yellow");
+                    break;
+                case 4:
+                    Console.WriteLine("Green");
+                    break;
+                case 5:
+                    Console.WriteLine("Light Blue");
+                    break;
+                case 6:
+                    Console.WriteLine("Blue");
+                    break;
+                case 7:
+                    Console.WriteLine("Violet");
+                    break;
+                default:
+                    Console.WriteLine("Write 1-7");
+                    break;
             }
-            else if (name == " ")
+
+            */
+
+
+            /*      //задание ДЗ №4
+            
+            Console.Write("Enter the temperature: ");
+
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            if (t > -5)
             {
-                Console.WriteLine("Empty name");
+                Console.WriteLine("Warm");
+            }
+            else if (t <= -5 && t > -20)
+            {
+                Console.WriteLine("Normal");
+            }
+            else if (t <= -20)
+            {
+                Console.WriteLine("Cold");
+            }
+            
+            */
+
+
+            /*      //задание ДЗ №3
+            
+            Console.Write("Enter the number: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a %2 == 0)
+            {
+                Console.WriteLine("This number is EVEN");
             }
             else
             {
-                Console.Write("Learn and practise, ");
-                Console.Write(name);
-                Console.WriteLine(" ;)");
+                Console.WriteLine("This number is ODD");
             }
 
-            Console.Write("Enter your weight : ");
-            int weight = Convert.ToInt32(Console.ReadLine());
+            */
 
-            string nickname = "Tomas";
 
-            switch (nickname)
+            /*      //задание ДЗ №2
+            
+            Console.Write("Enter the number of month: ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (a == 1 || a == 2 || a == 12)
             {
-                case "Nikita":
-                    Console.WriteLine("Your name is Nikita");
+                Console.WriteLine("Winter");
+            }
+            else if (a == 3 || a == 4 || a == 5) 
+            {
+                Console.WriteLine("Spring");
+            }
+            else if (a == 6 || a == 7 || a == 8)
+            {
+                Console.WriteLine("Spring");
+            }
+            else if (a == 9 || a == 10 || a == 11)
+            {
+                Console.WriteLine("Spring");
+            }
+            else 
+            {
+                Console.WriteLine("Wrong number of month");
+            }
+
+            */
+
+
+            /*              //задание ДЗ №1
+            switch (a) 
+            {
+                case 1:
+                case 2:
+                case 12:
+                    Console.WriteLine("Winter");
                     break;
-                case "Tomas":
-                    Console.WriteLine("Your name is Tomas");
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Autumn");
                     break;
                 default:
-                    Console.WriteLine("default");
+                    Console.WriteLine("Write 1-12");
                     break;
             }
+            */
 
-           
-            Console.WriteLine(Math.Pow(5, 2));
-            
+
+            /*    int a = 23;
+                string str = "Slovo";
+                bool b = true;
+
+                double c = 18;
+                int d = 5;
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine(str);
+                a = a + 23;
+                Console.WriteLine(a);
+                //Console.WriteLine("Hello, World!(1)");//
+                Console.WriteLine("Hello, World!(2)");
+
+                string json = JsonConvert.SerializeObject(str);
+
+                Console.Write("Multiply of numbers:");
+                Console.WriteLine(c * d);
+                Console.WriteLine(c / d);
+                Console.Write("Hello");
+                Console.WriteLine(" World!(3)");
+
+
+
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();  // вводим имя
+                if (name == "")
+                {
+                    Console.WriteLine("Empty name");
+                }
+                else if (name == " ")
+                {
+                    Console.WriteLine("Empty name");
+                }
+                else
+                {
+                    Console.Write("Learn and practise, ");
+                    Console.Write(name);
+                    Console.WriteLine(" ;)");
+                }
+
+                Console.Write("Enter your weight : ");
+                int weight = Convert.ToInt32(Console.ReadLine());
+
+                string nickname = "Tomas";
+
+                switch (nickname)
+                {
+                    case "Nikita":
+                        Console.WriteLine("Your name is Nikita");
+                        break;
+                    case "Tomas":
+                        Console.WriteLine("Your name is Tomas");
+                        break;
+                    default:
+                        Console.WriteLine("default");
+                        break;
+                }
+
+
+                Console.WriteLine(Math.Pow(5, 2));
+            */
+
 
         }
-    
+
     }
         
 }
